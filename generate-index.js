c3e8f99444ce1c8798fd85e041ab5d7568d0e4e3
Anup Jayant Dharangutti <anup.dharangutti15@gmail.com>
2025-10-26 06:24:54 +0530

    Update generate-index.js

--- generate-index.js
+++ generate-index.js
@@ -1,6 +1,11 @@
-// scripts/generate-index.js
-const fs = require('fs');
-const path = require('path');
+// generate-index.js (ESM version)
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Resolve __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const notebookDir = path.join(__dirname, './notebook');
 const files = fs.readdirSync(notebookDir)
